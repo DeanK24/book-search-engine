01f90254f1e4285f720563b0e2dc52e08cef2a90
Dean Ketchmark <deank242@gmail.com>
2022-07-17 10:22:25 -0400

    finished routes but getting error

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -21,6 +21,15 @@ type User {
     savedBooks: [Book]
 }
 
+input savedBook {
+    description: String
+    title: String
+    bookId: String
+    image: String
+    link: String
+    authors: String
+}
+
 type Query {
     me: User
 }
